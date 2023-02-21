f86f63eea901d1d3269f9bbff0eb2534c7aec268
Austin Hargis <theaustinhargis@gmail.com>
2023-02-20 21:02:29 -0500

    Added 'Add' and 'Delete' buttons.

--- src/main.py
+++ src/main.py
@@ -21,12 +21,20 @@ class Hammer(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title(f"hammer - {version}")
+        self.minsize(400, 300)
+        self.db = Database("hammer.db")
         menu_bar = MenuBar(self)
         self.config(menu=menu_bar)
+        self.window()
+
+    def window(self):
+        add_button = tk.Button(self, text="Add", command=self.db.addQuery)
+        add_button.pack()
+
+        delete_button = tk.Button(self, text="Delete", command=self.db.deleteQuery)
+        delete_button.pack()
 
 
 if __name__ == "__main__":
     root = Hammer()
     root.mainloop()
-    
-    db = Database("hammer.db")
