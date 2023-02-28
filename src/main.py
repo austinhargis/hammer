78632560c9874ca6c637e224b7230c0b252cf11a
Austin Hargis <theaustinhargis@gmail.com>
2023-02-28 14:39:27 -0500

    Added populate_table function, which generates a visual table of the information that has been added to the database.

--- src/main.py
+++ src/main.py
@@ -27,15 +27,31 @@ class Hammer(tk.Tk):
         self.config(menu=menu_bar)
         self.window()
 
+    """
+        populate_table takes the return value of self.db.get_all_query()
+        and builds a "table" of tk.Entry with the database
+    """
+
+    def populate_table(self):
+        current_table = self.db.get_all_query()
+        print(current_table)
+
+        for y in range(len(current_table)):
+            for x in range(len(current_table[y])):
+                # if x > 0:
+                entry = tk.Entry(self, width=16, font=('Arial', 14,))
+                entry.grid(row=y, column=x)
+                entry.insert(tk.END, current_table[y][x])
+
     def window(self):
         add_button = tk.Button(self, text="Add", command=lambda: self.db.add_query())
-        add_button.pack()
+        add_button.grid()
 
         delete_button = tk.Button(self, text="Delete", command=lambda: self.db.delete_query())
-        delete_button.pack()
+        delete_button.grid()
 
-        get_all_button = tk.Button(self, text="Get All", command=lambda: self.db.get_all_query())
-        get_all_button.pack()
+        get_all_button = tk.Button(self, text="Get All", command=lambda: self.populate_table())
+        get_all_button.grid()
 
 
 if __name__ == "__main__":
