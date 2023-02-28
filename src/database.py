d38e0a851b9a53f1876a1fb9adcd9553779b96e4
Austin Hargis <theaustinhargis@gmail.com>
2023-02-28 10:44:29 -0500

    Added "Get All" button. Added filler queries for add_query and delete_query. Created get_all_query function. Updated inventory table to have an AUTOINCREMENT primary key.

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import random
 
 
 class Database:
@@ -8,27 +9,37 @@ class Database:
         if filename not in os.listdir():
             self.dbConnection = sqlite3.connect(filename)
             self.dbCursor = self.dbConnection.cursor()
-            self.dbCursor.execute(f"CREATE TABLE inventory(id integer primary key, "
-                                  "title varchar, "
-                                  "author varchar, "
-                                  "publish_date varchar, "
-                                  "type varchar, "
-                                  "location varchar, "
-                                  "quantity varchar)")
-            pass
+            self.dbCursor.execute(f"CREATE TABLE inventory("
+                                  f"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                  f"title varchar, "
+                                  f"author varchar, "
+                                  f"publish_date varchar, "
+                                  f"type varchar, "
+                                  f"location varchar, "
+                                  f"quantity varchar)")
+            self.dbConnection.commit()
 
         else:
             self.dbConnection = sqlite3.connect(filename)
             self.dbCursor = self.dbConnection.cursor()
 
-    # TODO: implement add query 
+    # Currently adds filler data to the table
     def add_query(self):
-        pass
+        data = ["Test", "Test2", "Test3", "Test4", "Test5", "Test6"]
+        self.dbCursor.execute(f"INSERT INTO inventory (title, author, publish_date, type, location, quantity)"
+                              f"VALUES (?, ?, ?, ?, ?, ?)", data)
+        self.dbConnection.commit()
 
-    # TODO: implement delete query
+    # Currently drops all data from the inventory table
     def delete_query(self):
-        pass
+        self.dbCursor.execute("DELETE FROM inventory")
+        self.dbConnection.commit()
 
     # TODO: implement update query
     def update_query(self):
         pass
+
+    # Returns all rows in the inventory table
+    def get_all_query(self):
+        self.dbCursor.execute("SELECT * FROM inventory")
+        return self.dbCursor.fetchall()
