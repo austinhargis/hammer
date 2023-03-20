9d801ac5279f40d24313f7462363538df46b8c72
Austin Hargis <theaustinhargis@gmail.com>
2023-03-19 21:41:20 -0400

    Updated menu_bar.py to allow referencing of file menu attributes from outside of the class.

--- src/menu_bar.py
+++ src/menu_bar.py
@@ -8,18 +8,16 @@ class MenuBar(tk.Menu):
 	def __init__(self, parent):
 		tk.Menu.__init__(self, parent)
 
-		file_menu = tk.Menu(self, tearoff=False)
-		self.add_cascade(label="File", underline=0, menu=file_menu)
-		file_menu.add_command(label="Exit", underline=1, command=self.quit)
+		self.file_menu = tk.Menu(self, tearoff=False)
+		self.add_cascade(label="File", underline=0, menu=self.file_menu)
+		self.file_menu.add_command(label="Exit", underline=1, command=self.quit)
 
-		database_menu = tk.Menu(self, tearoff=False)
-		self.add_cascade(label="Database", underline=0, menu=database_menu)
-		database_menu.add_command(label="Save DB", underline=1)
-		database_menu.add_command(label="Open DB", underline=1)
+		self.database_menu = tk.Menu(self, tearoff=False)
+		self.add_cascade(label="Database", underline=0, menu=self.database_menu)
 
-		help_menu = tk.Menu(self, tearoff=False)
-		self.add_cascade(label="Help", underline=0, menu=help_menu)
-		help_menu.add_command(label="GitHub", underline=1, command=lambda: open_new_tab(
+		self.help_menu = tk.Menu(self, tearoff=False)
+		self.add_cascade(label="Help", underline=0, menu=self.help_menu)
+		self.help_menu.add_command(label="GitHub", underline=1, command=lambda: open_new_tab(
 			'https://github.com/austinhargis/hammer'))
 
 	def quit(self):
