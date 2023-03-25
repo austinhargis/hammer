511e7a4bb42f103c281ac70590fe92b3f5941a52
Austin Hargis <theaustinhargis@gmail.com>
2023-03-25 17:44:51 -0400

    The 'Manage' window now populates with items from the selected entry. The first entry in the database is now selected by default on launch.

--- src/manage_window.py
+++ src/manage_window.py
@@ -8,7 +8,11 @@ class ManageItem:
     def __init__(self, parent):
         super().__init__()
 
+        self.parent = parent
+
         self.template = ItemInfo(parent, 'manage')
+
+        self.fill_entries()
 
         self.template.bind('<Return>', lambda event: parent.add_entry([self.template.title_text.get(),
                                                                        self.template.author_text.get(),
@@ -31,3 +35,13 @@ class ManageItem:
         tk.Button(button_frame, text='Cancel', command=lambda: self.template.destroy()).pack(side='right')
 
         self.template.mainloop()
+
+    def fill_entries(self):
+        current_item = self.parent.tree.focus()
+
+        self.template.title_text.insert(0, self.parent.tree.item(current_item)['values'][1])
+        self.template.author_text.insert(0, self.parent.tree.item(current_item)['values'][2])
+        self.template.publish_date_text.insert(0, self.parent.tree.item(current_item)['values'][3])
+        self.template.type_text.insert(0, self.parent.tree.item(current_item)['values'][4])
+        self.template.location_text.insert(0, self.parent.tree.item(current_item)['values'][5])
+        self.template.quantity_text.insert(0, self.parent.tree.item(current_item)['values'][6])
