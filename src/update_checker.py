0e6dd27f16cadb47780270e193e447b0f0293055
Austin Hargis <theaustinhargis@gmail.com>
2023-03-29 14:29:27 -0400

    Adjusted the layout of the update manager. Added a button that takes the user to the releases page.

--- src/update_checker.py
+++ src/update_checker.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import urllib.request
+import webbrowser
+from datetime import datetime
 
 
 class UpdateChecker(tk.Toplevel):
@@ -7,32 +9,34 @@ class UpdateChecker(tk.Toplevel):
     def __init__(self, parent):
         super().__init__()
 
-        self.current_version = None
+        # Passed parameters
         self.parent = parent
+
+        # Initialize variables with default values
+        self.server_version_text = None
+        self.update_button = None
+        self.current_version = None
         self.server_version = None
         self.version = '0.1.0'
 
         self.attributes('-topmost', True)
         self.resizable(False, False)
-        self.title('Updater')
+        self.title('Check for Updates')
 
         title_frame = tk.Frame(self)
         title_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2,
                          pady=(self.parent.padding * 2, self.parent.padding))
-        tk.Label(title_frame, text='Update').pack()
-
-        auto_update_frame = tk.Frame(self)
-        auto_update_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2, pady=self.parent.padding)
-        tk.Label(auto_update_frame, text=f'Automatic Updates {self.parent.save_m.data["automatic_updates"]}').pack()
+        tk.Label(title_frame, text='Update Checker').pack(side='left')
 
         subtitle_frame = tk.Frame(self)
         subtitle_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2, pady=self.parent.padding)
-        tk.Label(subtitle_frame, text=f'Last Checked {self.parent.save_m.data["last_update_check"]}').pack()
+        self.last_check = tk.Label(subtitle_frame, text=f'Last Checked {self.parent.save_m.data["last_update_check"]}')
+        self.last_check.pack(side='left')
 
         self.server_label = tk.Label(self, text='Server Version: ')
-        self.server_label.pack()
+        self.server_label.pack(side='left')
 
-        tk.Label(self, text=f'Current Version: {self.version}').pack()
+        tk.Label(self, text=f'Current Version: {self.version}').pack(side='right')
 
         check_update_theme = tk.Frame(self)
         check_update_theme.pack(fill='both', expand=True, padx=self.parent.padding * 2, pady=(self.parent.padding,
@@ -44,17 +48,30 @@ class UpdateChecker(tk.Toplevel):
         self.mainloop()
 
     def check_for_update(self):
+        self.parent.save_m.data['last_update_check'] = datetime.now().strftime('%I:%M%p %m/%d/%Y')
+        self.last_check.configure(text=f'Last Checked {self.parent.save_m.data["last_update_check"]}')
+
+        # Download the latest version file from the server
         urllib.request.urlretrieve('https://raw.githubusercontent.com/austinhargis/hammer/main/version.txt',
                                    '../version.txt')
 
+        # Convert the current and server version to information that is usable
         if self.server_version is None:
             file = open('../version.txt', 'r')
             self.server_version_text = file.read()
-            self.server_version = tuple(self.server_version_text.split('.'))
+            self.server_version = tuple(self.server_version_text.replace('\n', '').split('.'))
             self.current_version = tuple(self.version.split('.'))
             file.close()
 
         self.server_label['text'] = f'Server Version: {self.server_version_text}'
 
+        # Determine whether the server version is newer than the current version
         if self.server_version > self.current_version:
-            print(f'update needed')
+            if self.update_button is None:
+                self.update_button = tk.Button(self,
+                                               text='Get Update',
+                                               command=lambda: webbrowser.open_new_tab(
+                                                   'https://github.com/austinhargis/hammer/releases')
+                                               )
+                self.update_button.pack(padx=self.parent.padding * 2,
+                                        pady=(self.parent.padding, self.parent.padding * 2))
