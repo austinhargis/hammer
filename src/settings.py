bbc531806f0115930ca53e45520a18a23f05dd99
Austin Hargis <theaustinhargis@gmail.com>
2023-03-31 19:56:44 -0400

    Updated Settings screen to use ttk buttons and labels.

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ class SettingsWindow(tk.Toplevel):
         theme_frame = tk.Frame(self)
         theme_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2,
                          pady=(self.parent.padding * 2, self.parent.padding))
-        tk.Label(theme_frame, text='Theme').pack(side='left')
+        ttk.Label(theme_frame, text='Theme').pack(side='left')
         self.theme_var = tk.StringVar(theme_frame)
         self.theme_var.set(self.parent.save_m.data['theme'].capitalize())
         theme_dropdown = tk.OptionMenu(theme_frame, self.theme_var, *['Dark', 'Light', 'System'])
@@ -24,7 +24,7 @@ class SettingsWindow(tk.Toplevel):
 
         update_frame = tk.Frame(self)
         update_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2, pady=self.parent.padding)
-        tk.Label(update_frame, text='Automatic Update').pack(side='left')
+        ttk.Label(update_frame, text='Automatic Update').pack(side='left')
         self.update_var = tk.StringVar(update_frame)
         self.update_var.set(self.parent.save_m.data['automatic_updates'].capitalize())
         update_dropdown = tk.OptionMenu(update_frame, self.update_var, *['Enabled', 'Disabled'])
@@ -33,7 +33,7 @@ class SettingsWindow(tk.Toplevel):
 
         developer_frame = tk.Frame(self)
         developer_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2, pady=self.parent.padding)
-        tk.Label(developer_frame, text='Show Developer Menu').pack(side='left')
+        ttk.Label(developer_frame, text='Show Developer Menu').pack(side='left')
         self.developer_var = tk.StringVar(developer_frame)
         self.developer_var.set(self.parent.save_m.data['show_developer_menu'].capitalize())
         developer_dropdown = tk.OptionMenu(developer_frame, self.developer_var, *['Enabled', 'Disabled'])
@@ -43,9 +43,9 @@ class SettingsWindow(tk.Toplevel):
         save_settings_frame = tk.Frame(self)
         save_settings_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2, pady=(self.parent.padding,
                                                                                                self.parent.padding * 2))
-        tk.Button(save_settings_frame, text='Save Settings', command=lambda: self.prepare_for_save()).pack()
+        ttk.Button(save_settings_frame, text='Save Settings', command=lambda: self.prepare_for_save()).pack()
 
-        tk.Label(self, text=self.parent.save_m.data['settings_version']).pack()
+        ttk.Label(self, text=self.parent.save_m.data['settings_version']).pack()
 
         self.mainloop()
 
