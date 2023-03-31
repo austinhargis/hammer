5175636d3c352195d080da966c0a3c808bd44b76
Austin Hargis <theaustinhargis@gmail.com>
2023-03-31 19:57:18 -0400

    Updated add_window to use ttk buttons and labels.

--- src/add_window.py
+++ src/add_window.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 from item_info import ItemInfo
 
@@ -12,14 +13,14 @@ class AddItem:
 
         self.template = ItemInfo(self.parent, 'add')
 
-        self.template.bind('<Return>', lambda event: parent.add_entry(self.template.get_item_info(), self))
+        # self.template.bind('<Return>', lambda event: parent.add_entry(self.template.get_item_info(), self))
 
         button_frame = tk.Frame(self.template)
         button_frame.pack(expand=True, padx=self.parent.padding * 2,
                           pady=(self.parent.padding, self.parent.padding * 2))
-        tk.Button(button_frame, text='Add Item', command=lambda: self.parent.add_entry(self.template.get_item_info(),
+        ttk.Button(button_frame, text='Add Item', command=lambda: self.parent.add_entry(self.template.get_item_info(),
                                                                                        self)) \
             .pack(side='left')
-        tk.Button(button_frame, text='Cancel', command=lambda: self.template.destroy()).pack(side='right')
+        ttk.Button(button_frame, text='Cancel', command=lambda: self.template.destroy()).pack(side='right')
 
         self.template.mainloop()
