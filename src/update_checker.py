fe9b60516928306691c9d19e01eb2366304a54d1
Austin Hargis <theaustinhargis@gmail.com>
2023-03-31 18:57:39 -0400

    Refactored update_checker.

--- src/update_checker.py
+++ src/update_checker.py
@@ -8,20 +8,48 @@ class UpdateChecker(tk.Toplevel):
 
     def __init__(self, parent):
         super().__init__()
-
-        # Passed parameters
-        self.parent = parent
-
-        # Initialize variables with default values
-        self.server_version_text = None
-        self.update_button = None
-        self.current_version = None
-        self.server_version = None
-        self.version = '0.0.0'
-
         self.attributes('-topmost', True)
         self.resizable(False, False)
         self.title('Check for Updates')
+
+        self.parent = parent
+        self.server_version = None
+        self.update_button = None
+        self.version = '0.2.0'
+
+        self.window()
+        self.check_for_update()
+        self.mainloop()
+
+    def check_for_update(self):
+        self.parent.save_m.data['last_update_check'] = datetime.now().strftime('%I:%M%p %m/%d/%Y')
+        self.last_check.configure(text=f'Last Checked {self.parent.save_m.data["last_update_check"]}')
+
+        # Download the latest version file from the server
+        urllib.request.urlretrieve('https://raw.githubusercontent.com/austinhargis/hammer/main/version.txt',
+                                   './data/server_version.txt')
+
+        # Convert the current and server version to information that is usable
+        if self.server_version is None:
+            with open('./data/server_version.txt') as s_ver:
+                self.server_version_text = s_ver.read()
+            self.server_version = tuple(self.server_version_text.replace('\n', '').split('.'))
+            self.current_version = tuple(self.version.split('.'))
+
+        self.server_label['text'] = f'Server Version: {self.server_version_text}'
+
+        # Determine whether the server version is newer than the current version
+        if self.server_version > self.current_version:
+            if self.update_button is None:
+                self.update_button = tk.Button(self,
+                                               text='Get Update',
+                                               command=lambda: webbrowser.open_new_tab(
+                                                   f'https://github.com/austinhargis/hammer/releases/tag/version_{self.server_version_text}')
+                                               )
+                self.update_button.pack(padx=self.parent.padding * 2,
+                                        pady=(self.parent.padding, self.parent.padding * 2))
+
+    def window(self):
 
         title_frame = tk.Frame(self)
         title_frame.pack(fill='both', expand=True, padx=self.parent.padding * 2,
@@ -42,36 +70,3 @@ class UpdateChecker(tk.Toplevel):
         check_update_theme.pack(fill='both', expand=True, padx=self.parent.padding * 2, pady=(self.parent.padding,
                                                                                               self.parent.padding * 2))
         tk.Button(check_update_theme, text='Check for Updates', command=lambda: self.check_for_update()).pack()
-
-        self.check_for_update()
-
-        self.mainloop()
-
-    def check_for_update(self):
-        self.parent.save_m.data['last_update_check'] = datetime.now().strftime('%I:%M%p %m/%d/%Y')
-        self.last_check.configure(text=f'Last Checked {self.parent.save_m.data["last_update_check"]}')
-
-        # Download the latest version file from the server
-        urllib.request.urlretrieve('https://raw.githubusercontent.com/austinhargis/hammer/main/version.txt',
-                                   './data/version.txt')
-
-        # Convert the current and server version to information that is usable
-        if self.server_version is None:
-            file = open('./data/version.txt', 'r')
-            self.server_version_text = file.read()
-            self.server_version = tuple(self.server_version_text.replace('\n', '').split('.'))
-            self.current_version = tuple(self.version.split('.'))
-            file.close()
-
-        self.server_label['text'] = f'Server Version: {self.server_version_text}'
-
-        # Determine whether the server version is newer than the current version
-        if self.server_version > self.current_version:
-            if self.update_button is None:
-                self.update_button = tk.Button(self,
-                                               text='Get Update',
-                                               command=lambda: webbrowser.open_new_tab(
-                                                   f'https://github.com/austinhargis/hammer/releases/tag/version_{self.server_version_text}')
-                                               )
-                self.update_button.pack(padx=self.parent.padding * 2,
-                                        pady=(self.parent.padding, self.parent.padding * 2))
