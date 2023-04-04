4a789e638dd57525d7f2ed6a71a99fee8d64129b
Austin Hargis <theaustinhargis@gmail.com>
2023-04-04 19:12:26 -0400

    Changed the padding for a frame in the add window.

--- src/add_window.py
+++ src/add_window.py
@@ -14,8 +14,8 @@ class AddItem(ItemInfo):
         # self.template.bind('<Return>', lambda event: parent.add_entry(self.template.get_item_info(), self))
 
         button_frame = tk.Frame(self)
-        button_frame.pack(expand=True, padx=self.parent.padding * 2,
-                          pady=(self.parent.padding, self.parent.padding * 2))
+        button_frame.pack(expand=True, padx=self.parent.padding,
+                          pady=self.parent.padding)
         ttk.Button(button_frame,
                    text='Add Item',
                    command=lambda: [self.parent.add_entry(self.get_item_info()),
